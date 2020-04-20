c88e8fcabe0919705c003b0a97e8610caf2d03b7
Calvin <calvin.mcconnell20@gmail.com>
2020-04-19 22:59:35 -0700

    update more styling, remove hardcoded song

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
+      redirect_to root_url
     else
       flash.now[:alert] = "Username or password is invalid"
       render "new"
